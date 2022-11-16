98e8fa5e64dd5c158cbeb719db552d2681f2603e
TheMorbidArk <1482506841@qq.com>
2022-11-16 18:11:01 +0800

    Revert "Feat : 修正Log输出方式"
    This reverts commit 7c8dac96f8a21aafcb2ba920596aa2200b2fc18a.
    
    # Conflicts:
    #	Cli/cli.c

--- Cli/cli.c
+++ Cli/cli.c
@@ -8,41 +8,49 @@
 
 //执行脚本文件
 static void runFile(const char *path) {
-    const char *lastSlash = strrchr(path, '/');
-    if (lastSlash != NULL) {
-        char *root = (char *) malloc(lastSlash - path + 2);
-        memcpy(root, path, lastSlash - path + 1);
-        root[lastSlash - path + 1] = '\0';
-        rootDir = root;
-    }
+	const char *lastSlash = strrchr(path, '/');
+	if (lastSlash != NULL) {
+		char *root = (char *)malloc(lastSlash - path + 2);
+		memcpy(root, path, lastSlash - path + 1);
+		root[lastSlash - path + 1] = '\0';
+		rootDir = root;
+	}
 
-    VM *vm = newVM();
-    const char *sourceCode = readFile(path);
-    executeModule(vm, OBJ_TO_VALUE(newObjString(vm, path, strlen(path))), sourceCode);
+	VM *vm = newVM();
+	const char *sourceCode = readFile(path);
+	executeModule(vm, OBJ_TO_VALUE(newObjString(vm, path, strlen(path))), sourceCode);
 }
 
 void LOGO() {
     char *logWord = LOG_VERSION;
     printf("%s", logWord);
 
+	printf("%s",i);
+	printf("%s",a);
+	printf("%s",b);
+	printf("%s",c);
+	printf("%s",d);
+	printf("%s",e);
+	printf("%s",f);
+	printf("%s",g);
 }
 
 int main(int argc, const char **argv) {
 
-    clock_t start_t, finish_t;
+	clock_t start_t,finish_t;
 
-    LOGO();
+	LOGO();
 
     if (argc == 1) { ;
     } else {
 
-        start_t = clock();
+		start_t = clock();
 
-        runFile(argv[1]);
+		runFile(argv[1]);
 
-        finish_t = clock();
+		finish_t = clock();
 
-        printf("> CPU Run Time: %lfs <\r\n", (double) (finish_t - start_t) / CLOCKS_PER_SEC);
-    }
-    return 0;
+		printf("> CPU Run Time：%lfs\r\n", (double)(finish_t - start_t) / CLOCKS_PER_SEC);
+	}
+	return 0;
 }
