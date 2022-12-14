8602224e8d88173a7009be7b86a8aa11de738cd0
MorbidArk <1482506841@qq.com>
2022-12-14 12:24:09 +0800

    Feat : 初步实现 CLI 自动补全

--- Cli/cli.h
+++ Cli/cli.h
@@ -33,17 +33,45 @@ void LOGO() {
 /* 添加自动补全信息 */
 void completion(const char *buf, linenoiseCompletions *lc) {
     /* TODO 添加关键字及内置函数自动补全信息 */
-    if (buf[0] == 'h') {
-        linenoiseAddCompletion(lc,"hello");
-        linenoiseAddCompletion(lc,"hello there");
+    if (buf[0] == 'T') {
+        linenoiseAddCompletion(lc, "Tide");
+    } else if (buf[0] == 'f') {
+        linenoiseAddCompletion(lc, "false");
+        linenoiseAddCompletion(lc, "for");
+        linenoiseAddCompletion(lc, "fun");
+    } else if (buf[0] == 'i') {
+        linenoiseAddCompletion(lc, "if");
+        linenoiseAddCompletion(lc, "is");
+        linenoiseAddCompletion(lc, "import");
+    } else if (buf[0] == 's') {
+        linenoiseAddCompletion(lc, "static");
+        linenoiseAddCompletion(lc, "super");
+    } else if (buf[0] == 'e') {
+        linenoiseAddCompletion(lc, "elif");
+        linenoiseAddCompletion(lc, "else");
+    } else if (buf[0] == 't') {
+        linenoiseAddCompletion(lc, "true");
+        linenoiseAddCompletion(lc, "this");
+    } else if (buf[0] == 'w') {
+        linenoiseAddCompletion(lc, "while");
+    } else if (buf[0] == 'b') {
+        linenoiseAddCompletion(lc, "break");
+    } else if (buf[0] == 'c') {
+        linenoiseAddCompletion(lc, "continue");
+        linenoiseAddCompletion(lc, "class");
+    } else if (buf[0] == 'r') {
+        linenoiseAddCompletion(lc, "return");
+    } else if (buf[0] == 'n') {
+        linenoiseAddCompletion(lc, "null");
     }
     /* TODO 添加变量及函数实时自动补全 */
 }
+
 /* 命令提示内容显示设置 */
 char *hints(const char *buf, int *color, int *bold) {
     /* TODO 添加命令提示信息 */
     // 如果命令为 hello
-    if (!strcasecmp(buf,"hello")) {
+    if (!strcasecmp(buf, "hello")) {
         // 命令字体颜色
         *color = 35;
         // 命令字体样式
